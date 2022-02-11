43135402186f7e6ea042037ea8f08477a65cb960
max <maximiliangruber91@gmail.com>
2022-02-11 10:49:48 +0100

    Heroku Files

--- micrometeorite-model-rest-api-service.py
+++ micrometeorite-model-rest-api-service.py
@@ -4,9 +4,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/predictionMerkelVoigt', methods=['POST'])
+@app.route('/micrometeorite-prediction/api/v1/predictionMerkelVoigt', methods=['POST'])
 def predict():
-    print(request.json)
     return jsonify({"result": prediction(request.json["file"])}), 200
 
 if __name__ == '__main__':
--- prediction.py
+++ prediction.py
@@ -3,22 +3,32 @@ import cv2
 import numpy as np
 import pandas as pd
 from keras.models import model_from_json
+from tensorflow import keras
+
 
 def prediction(base64Picture):
-    json_file = open('model2.json', 'r')
+    images = []
+    shape = (128, 128)
+    # Load Model
+    json_file = open('model5.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     best_model = model_from_json(loaded_model_json)
-    images = []
-    labels = []
-    shape = (128, 128)
+    # load weights into new model
+    best_model.load_weights("model5.h5")
+
+    opt = keras.optimizers.Adam(learning_rate=1e-05)
+
+    best_model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=['acc'])
+    # Prepare Image
     source = base64.b64decode(base64Picture[0])
     npimg = np.fromstring(source, dtype=np.uint8);
     img = cv2.imdecode(npimg, 1)
-    labels.append(1)
     img = cv2.resize(img, shape)
     images.append(img)
     images = np.array(images) / 255
+    # Prediction
     predictions = best_model.predict(images)
     predictions = pd.DataFrame(predictions, columns=['Nicht-MM', 'MM'])
+    print(predictions)
     return str(predictions['MM'][0])
